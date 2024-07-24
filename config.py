180f4fe3e216880929390f1738d62b975050a2b8
adityabingi <adityabingi786@gmail.com>
2024-07-24 12:56:53 +0530

    utility for hyper-params

---
+++ config.py
@@ -0,0 +1,29 @@
+class Config:
+    
+    # training params 
+    lr = 0.0002
+    num_epochs = 1000
+    batch_size = 128
+    lr_schedule = "cosine"
+    warmup_steps = ""
+    
+    # Data Params
+    data_dir = "/home/adithya/celeba/"
+    img_size = [3, 64, 64]    # [C, H, W]
+    
+    # data loader worker threads
+    num_workers = 3
+
+    # diffusion params
+    diffusion_timesteps = 1000
+    
+    
+    # multi-gpu params
+    port = 8888
+    
+    ckpt_dir = "ckpts/"
+    log_dir = "logs/"
+    
+    # random seed
+    seed = 42
+    
